66693f78d0620466f73b9fd517342e96bbd9135f
Renz Iver Baliber <renziverdb@gmail.com>
2020-01-29 13:46:39 +0800

    chore: update the dataset and retrain LDA

---
+++ corona-topic-modeling/scraper.py
@@ -0,0 +1,28 @@
+import json
+import yaml
+from datetime import date
+from twitterscraper import query_tweets
+
+def config_args(config):
+    """Return arguments from the configuration file.
+    Keyword arguments:
+    config -- the configuration file that contains the parameters needed for the program.
+    """
+    with open('config.yaml', 'rb') as file:
+        conf = yaml.safe_load(file)
+    return conf['scrape']
+
+if __name__ == '__main__':
+    args = config_args('config.yaml')
+    query_string = args['query']
+    print(query_string)
+    limit = args['limit']
+    b_date = date.today()
+    e_date = date.today()
+    tweets = []
+    for tweet in query_tweets(query_string, limit):
+        tweets.append(tweet.text)
+    
+    with open('datasets/data-2.json', 'a') as f:
+        f.write(json.dumps(tweets))
+
